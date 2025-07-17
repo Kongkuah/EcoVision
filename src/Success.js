c7ae110a9653ddadb197e64923d5d7e2e6cbfa1e
Maxwell Kongkuah <maxwellkongkuah@gmail.com>
2025-07-18 00:49:19 +0300

    Update site and add BIMI SVG logo

---
+++ src/Success.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Success() {
+  return (
+    <div className="success-message">
+      <h2>Thank you!</h2>
+      <p>Your message has been sent successfully. We will get back to you soon.</p>
+    </div>
+  );
+}
+
+export default Success;

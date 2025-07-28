967f69a960d00e569223dc0a91e36969bfaa2cd2
Maxwell Kongkuah <maxwellkongkuah@gmail.com>
2025-07-28 18:59:07 +0300

    Update all

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function Home() {
           <p>
             <strong>Office:</strong> Accra, Ghana<br />
             <strong>Email:</strong> info@ecovisiongh.com<br />
-            <strong>Phone:</strong> +233 24 762 3464
+            <strong>Phone:</strong> 
           </p>
           <form
             name="contact"
